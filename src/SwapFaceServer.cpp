48a47bd4f80a0e9c5738d6fa1b1bc80f563187b5
Antoine <saintpere.antoine@hotmail.com>
2020-03-21 14:57:29 +0100

    add tuto learning

--- src/SwapFaceServer.cpp
+++ src/SwapFaceServer.cpp
@@ -1,3 +1,5 @@
+#include <cpprest/http_client.h>
+#include <cpprest/filestream.h>
 #include <cpprest/http_listener.h>              // HTTP server
 #include <cpprest/json.h>                       // JSON library
 #include <cpprest/uri.h>                        // URI library
@@ -11,6 +13,10 @@ using namespace web;
 using namespace http;
 using namespace utility;
 using namespace http::experimental::listener;
+
+using namespace web::http;                  // Common HTTP functionality
+using namespace web::http::client;          // HTTP client features
+using namespace concurrency::streams; 
 
 class SwapFaceServer {
 private:
@@ -60,8 +66,35 @@ void handle_delete(http_request message) {
 std::unique_ptr<SwapFaceServer>g_http;
 
 int main (int argc, char *argv[]) {
-    utility::string_t port= U("34568");
-    utility::string_t adress = U("http://localhost:");
+    //utility::string_t port= U("34568");
+    //utility::string_t adress = U("http://localhost:");
 
+    auto fileStream = std::make_shared<ostream>();
 
+    pplx::task<void> requestTask = fstream::open_ostream(U("results.html"))
+        .then([=] (ostream outFile) {
+            *fileStream = outFile;
+
+            http_client client(U("http://www.bing.com/"));
+
+            uri_builder builder(U("/search"));
+            builder.append_query(U("q"), U("cpprestsdk github"));
+            return client.request(methods::GET, builder.to_string());
+        })
+        .then([=] (http_response response) {
+            printf("Received response status code:%u\n", response.status_code());
+
+            return response.body().read_to_end(fileStream->streambuf());
+        })
+        .then([=](size_t) {
+            return fileStream->close();
+        });
+
+        try {
+            requestTask.wait();
+        }
+        catch (const std::exception &e) {
+            printf("Error exception:%s\n", e.what());
+        }
+        return 0;
 }
