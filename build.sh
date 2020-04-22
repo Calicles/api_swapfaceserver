abba1b763b92dc9b42ab86b0b89c4a7a455b3954
Antoine <saintpere.antoine@hotmail.com>
2020-04-22 11:51:41 +0200

    add comments

--- build.sh
+++
@@ -1,3 +0,0 @@
-cd build
-rm *
-g++ -std=c++11 -o main ../src/SwapFaceServer.cpp ../src/FaceSwapper.cpp -I../include -lcpprest -lssl -lcrypto -lboost_system -lpthread `pkg-config --cflags --libs opencv4`
